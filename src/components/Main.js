41a026ac4f5457bc02618ed7f349092d3f627aec
Oscar Li <oscarli@Oscars-MBP-3.hsd1.ca.comcast.net>
2019-03-26 12:10:19 -0700

    css

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Register} from './Register'
+
+export class Main extends React.Component{e
+    render() {
+        return(
+            <div className="main">
+                <Register />
+            </div>
+        );
+    }
+}
