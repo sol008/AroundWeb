243fd33779578869a6efdf6cbc5c73d466dc1f4f
Oscar Li <oscarli@Oscars-MacBook-Pro-3.local>
2019-03-27 17:37:15 -0700

    lolll

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Register} from './Register';
+import {Link, Route} from 'react-router-dom';
 import {Login} from './Login';
 import {Home} from './Home';
 
-export class Main extends React.Component{e
+export class Main extends React.Component{
     render() {
         return(
             <div className="main">
-                <Register />
-                <Login />
-                <Home /> 
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+                <Route path="/home" component={Home} />
+                <Link to="/register">Register</Link><br />
+                <Link to="/login">Login</Link><br />
+                <Link to="/home">Home</Link><br />
             </div>
         );
     }
