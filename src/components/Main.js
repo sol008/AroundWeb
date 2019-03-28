44d7aa13a0dcc8d5fbef5ee0b60acf76da9149ec
Oscar Li <oscarli@Oscars-MBP-3.hsd1.ca.comcast.net>
2019-03-27 17:51:24 -0700

    lollll

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import {Register} from './Register';
-import {Link, Route} from 'react-router-dom';
-import {Login} from './Login';
-import {Home} from './Home';
+import React from "react";
+import { Register } from "./Register";
+import { Link, Route, Switch } from "react-router-dom";
+import { Login } from "./Login";
+import { Home } from "./Home";
 
-export class Main extends React.Component{
-    render() {
-        return(
-            <div className="main">
-                <Route path="/register" component={Register} />
-                <Route path="/login" component={Login} />
-                <Route path="/home" component={Home} />
-                <Link to="/register">Register</Link><br />
-                <Link to="/login">Login</Link><br />
-                <Link to="/home">Home</Link><br />
-            </div>
-        );
-    }
+export class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+          <Route component={Login} />
+        </Switch>
+        
+        <Link to="/register">Register</Link>
+        <br />
+        <Link to="/login">Login</Link>
+        <br />
+        <Link to="/home">Home</Link>
+        <br />
+      </div>
+    );
+  }
 }
