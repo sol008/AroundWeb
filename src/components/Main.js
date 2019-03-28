dae4503cb59cd308a249327914af107ecaa3b6ef
Oscar Li <oscarli@Oscars-MacBook-Pro-3.local>
2019-03-27 17:06:01 -0700

    login

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import {Register} from './Register'
+import React from 'react';
+import {Register} from './Register';
+import {Login} from './Login';
+import {Home} from './Home';
 
 export class Main extends React.Component{e
     render() {
         return(
             <div className="main">
                 <Register />
+                <Login />
+                <Home /> 
             </div>
         );
     }
